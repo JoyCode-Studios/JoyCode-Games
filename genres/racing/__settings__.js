7e930eb0313ca1979bebba96e0ed4c96f236ac4b
OllieTheKingYT <macaulayfernandeschannel@gmail.com>
2025-10-08 19:56:20 +0100

    Release 1.4

---
+++ genres/racing/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2336948.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 256391046, 256391026, 256391038, 256391035, 256391022, 256391031, 256391034, 256391025, 256391049, 256391050, 256391055, 256391023, 256391187, 256391397 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/256391033/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/256391029/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/256391047/1/ammo.js', 'preload' : true},
+];
